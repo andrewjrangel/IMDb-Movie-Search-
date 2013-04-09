f685f8d33369f0b311acd9a893bd6257948d1e5b
Andrew Rangel <andrewjrangel@gmail.com>
2013-04-09 17:58:29 -0500

    Initial Commit

---
+++ NetworkingExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NetworkingExample
+//
+//  Created by Ventura Rangel on 4/9/13.
+//  Copyright (c) 2013 Andrews Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NetworkingExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NetworkingExample
+//
+//  Created by Ventura Rangel on 4/9/13.
+//  Copyright (c) 2013 Andrews Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
